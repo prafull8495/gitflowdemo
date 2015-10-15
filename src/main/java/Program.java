fea49af766aaae7478e77d5bc592ea567d613fbc
prbhosale <prbhosale@ebay.com>
2015-10-15 10:23:29 -0700

    Adding changes for feature set 3

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -1,7 +1,8 @@
 
 public class Program {
+	private static final String BRANCH_NAME = "feature_set_3";
 	public static void Main (String[] args) {
-		print("feature_set_1");
+		print(BRANCH_NAME);
 	}
 	private void print(String branchName) {}
 		System.out.println("This is the "+branchName+" branch!");
